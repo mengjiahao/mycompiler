760cff810f146d15da04715ea2dcbd10ea0f9d71
mjh-os <691225916@qq.com>
2015-06-16 16:50:30 +0800

    mjh-addsymbol/

--- include/symbol/Scope.h
+++ include/symbol/Scope.h
@@ -6,41 +6,112 @@
 #include <string>
 #include "Symbol.h"
 
+class Context;
+
 class Scope {
 
 public:
-    enum ScopeType {SCOPE_GLOBAL, SCOPE_CLASS, SCOPE_GLOBALFUNC, SCOPE_CLASSFUNC, SCOPE_LOCAL};
+    enum ScopeType {SCOPE_INVALIDE, SCOPE_GLOBAL, SCOPE_CLASS,
+    SCOPE_GLOBALFUNC, SCOPE_CLASSFUNC, SCOPE_LOCAL};
 
-    map<string, Symbol*> symbolMap;
-    list<Symbol *> symbolList;
+    int scopeId;
+    ScopeType scopeType;
+    string scopeName;
     int level;
     int startOffset;
-    int symbolNo;
+    int symbolsNo;
+    int totalByteSize;
 
     Scope *parent;
     Scope *superClass;
+    list<Scope *> childs;
+    map<string, Symbol *> symbolVarMap;
+    list<Symbol *> symbolSeqList;
 
-    static list<Symbol *> s_labelList;
-    static list<Symbol *> s_constvarList;
-    static list<Symbol *> s_tempvarList;
-    static list<Symbol *> s_symbolList;
+    static map<string, Symbol *> s_labelMap;
+    static map<string, Symbol *> s_constMap;
+    static map<string, Symbol *> s_tempVarMap;
+    static list<Symbol *> s_allSymbolList;
+    static map<int, Symbol*> s_allSymbolMap;
+
+    static Context* s_context;
 
 public:
-    Scope::ScopeType getScopeType();
-    void pushScope(Scope *newScope_t);
-    Scope* enterScope();
-    Scope* encloseScope();
-    Scope* getParentScope();
-    Scope* getSuperClassScope();
-    //void defineSymbol(Symbol *Symbol_t);
-    void searchSymbol(string symbolName_t);
+    Scope();
+    virtual ~Scope();
 
+    virtual void initGlobalScope();
+    virtual void initClassScope();
+    virtual void initGlobalFuncScope();
+    virtual void initClassFuncScope();
+    virtual void initLocalScope();
 
+    virtual void setScopeId();
+    virtual int getScopeId();
+
+    virtual void setScopeType(Scope::ScopeType scopeType_t);
+    virtual Scope::ScopeType  getScopeType();
+
+    virtual void setLevel(int level_t);
+    virtual int getlevel();
+
+    virtual void setStartOffset(int startOffset_t);
+    virtual int getStartOffset();
+
+    virtual void setSymbolsNo(int symbolsNo_t);
+    virtual int getSymbolsNo();
+
+    virtual void setTotalByteSize(int totalByteSize_t);
+    virtual int getTotalByteSize();
+
+    virtual void setParent(Scope *parent_t);
+    virtual Scope* getParent();
+
+    virtual void setSuperClass(Scope *superClass_t);
+    virtual Scope* getSuperClass();
+
+    virtual addChild(Scope *child_t);
+    virtual Scope* searchChild(string childName_t);
+
+    virtual addToSymbolVarMap(Symbol *symbolVar_t);
+    virtual Symbol* searchSymbolVarMap(string symbolName_t);
+
+    virtual addToSymbolSeqList(Symbol *symbolVar_t);
+    virtual list<Symbol *>& getSymbolSeqList();
+
+    static void addToLabelMap(Symbol *label_t);
+
+    static void addToConstantMap(Symbol *constant_t)
+
+    static void addToTempVarMap(Symbol *tempVar_t);
+
+    static void addToAllSymbolList(Symbol *symbol_t);
+
+    static void addToAllSymbolMap(Symbol *symbol_t);
+    static Symbol* searchAllSymbolMap(int symbolId_t);
+
+    virtual void pushScope(Scope *newScope_t);
+    virtual Scope* enterScope();
+    virtual Scope* encloseScope();
 };
 
 
-class
+/*****************************Context***********************************************/
 
+class Context {
+
+public:
+    Symbol* s_newSymbol;
+
+public:
+    Constext();
+    ~Constext();
+
+    static void clearNewSymbol();
+
+    static Context* getSingletonContext();
+
+};
 
 #endif
 
--- include/symbol/Symbol.h
+++ include/symbol/Symbol.h
@@ -4,15 +4,65 @@
 class Symbol {
 public:
 
-    //enum SymbolType {SYM_C};
+    enum SymbolType {SYMBOL_INVALIDE, SYMBOL_INTEGER_CONSTANT, SYMBOL_CHARACTER_CONSTANT,
+    SYMBOL_FLOATING_CONSTANT, SYMBOL_STRING_LITERAL, SYMBOL_TEMPVAR, SYMBOL_LABEL,
+    SYMBOL_VAR, SYMBOL_CLASSDECL, SYMBOL_FUNCDECL, SYMBOL_LABELDECL};
+
+    enum StorageType {STOR_INVALIDE = 0, STOR_EXTERN = 1, STOR_STATIC = 2,
+    STOR_AUTO = 4, STOR_REGISTER = 8};
+
+    enum TypeSfType {SF_INVALIDE = 0, SF_VOID = 1, SF_CHAR = 2, SF_SHORT = 4, SF_INT = 8,
+    SF_LONG = 16, SF_FLOAT = 32, SF_DOUBLE = 64, SF_SIGNED = 128, SF_UNSIGNED = 256};
+
+    enum TypeQfType {QF_INVALIDE = 0, QF_CONST = 1, QF_VOLATILE = 2, QF_VIRTUAL = 4};
 
     int symbolId;
     string symbolName;
+    SymbolType symbolType;
+    int storageType;
+    int typeSfType;
+    int typeQfType;
+
     int offset;
+    int byteSize;
 
-    int symbolType;
-    int attributes;
+    bool isArray;
+    int elemsNo;
 
+    Symbol();
+    virtual ~Symbol();
+
+    virtual void initSymbol(Symbol::SymbolType symbolType_t);
+
+    virtual void setSymbolId();
+    virtual int getSymbolId();
+
+    virtual void setSymbolName(string symbolName_t);
+    virtual string getSymbolName();
+
+    virtual void setSymbolType(Symbol::SymbolType symbolType_t);
+    virtual Symbol::SymbolType getSymbolType();
+
+    virtual void setStorageType(Symbol::StorageType storageType_t);
+    virtual int getStorageType();
+
+    virtual void setTypeSfType(Symbol::TypeSfType typeSfType_t);
+    virtual int getTypeSfType();
+
+    virtual void setTypeQfType(Symbol::TypeQfType typeQfType_t);
+    virtual int getTypeQfType();
+
+    virtual void setOffset(int offset_t);
+    virtual int getOffset();
+
+    virtual void setByteSize(int byteSize_t);
+    virtual int getByteSize();
+
+    virtual void setIsArray(bool isArray_t);
+    virtual bool getIsArray();
+
+    virtual void setElemsNo(int elemsNo);
+    virtual int getElemsNo();
 
 };
 
